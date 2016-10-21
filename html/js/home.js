c96522a8d6e1b2ed2a7716af7fe68eede143c74e
Ricardo-Silva91 <ricardos897@hotmail.com>
2016-10-21 20:03:20 +0000

    bck_web

--- html/js/home.js
+++ html/js/home.js
@@ -4,7 +4,7 @@
 $(document).ready(function () {
 
     var cookie = getCookie('MusicDB_token');
-    var url_rest = base_url_rest + 'numberOfAlbums?token=' + cookie;
+    var url_rest = base_url_rest + 'numberOfCars?token=' + cookie;
 
 
     $.ajax({
@@ -14,7 +14,7 @@ $(document).ready(function () {
         var json = data;
         if (json != null && json['op'] == 'success') {
             //alert(json['totalAlbums']);
-            $('#total-albums')[0].innerText = json['totalAlbums'];
+            $('#total-albums')[0].innerText = json['totalCars'];
         }
         else {
             //alert('something is wrong');
@@ -23,19 +23,19 @@ $(document).ready(function () {
         }
     });
 
-    $('#last-edited-album-title')[0].innerText = albums[albums.length-1]['title'];
-    $('#last-edited-album-artist')[0].innerText = albums[albums.length-1]['artist'];
+    $('#last-edited-album-title')[0].innerText = cars[cars.length-1]['model'];
+    $('#last-edited-album-artist')[0].innerText = cars[cars.length-1]['brand'];
 
     var elem = document.createElement('li');
     var elem_child1 = '<img src="" alt="User Image"><a class="users-list-name" href="#">Alexander Pierce</a><span class="users-list-date">Today</span>';//document.createElement('li');
 
 
 
-    for(var i = albums.length-1; i>albums.length-13; i--)
+    for(var i = cars.length-1; i>cars.length-13; i--)
     {
         elem = document.createElement('li');
-        //console.log(base_url_for_pics + albums[i]['pic_name']);
-        elem_child1 = '<a href="editAlbum.html?id=' + i + '"><img style="width: 60%; height: 50%" src=' + base_url_for_pics + albums[i]['pic_name'] + ' alt="User Image"> </a><a class="users-list-name" href="editAlbum.html?id=' + i + '">' + albums[i]['title'] + '</a><span class="users-list-date">' + albums[i]['artist'] + '</span>';//document.createElement('li');
+        //console.log(base_url_for_pics + cars[i]['pic_name']);
+        elem_child1 = '<a href="editAlbum.html?id=' + i + '"><img style="width: 60%; height: 50%" src=' + base_url_for_pics + cars[i]['pic_name'] + ' alt="User Image"> </a><a class="users-list-name" href="editAlbum.html?id=' + i + '">' + cars[i]['title'] + '</a><span class="users-list-date">' + cars[i]['artist'] + '</span>';//document.createElement('li');
         elem.innerHTML = elem_child1;
         $('#new_albums_painting')[0].appendChild(elem);
     }
