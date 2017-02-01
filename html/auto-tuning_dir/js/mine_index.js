b380a78555b3af0dc48f21dc3f1cf47587b2c4fb
Ricardo-Silva91 <ricardos897@hotmail.com>
2017-02-01 19:42:27 +0000

    public page pre release

--- html/auto-tuning_dir/js/mine_index.js
+++ html/auto-tuning_dir/js/mine_index.js
@@ -20,6 +20,8 @@ function filterCarsByBrand(brand) {
     }
 }
 
+
+
 function populateCarousel() {
 
     $($('#dg-container').children()[0]).html("");
@@ -27,14 +29,16 @@ function populateCarousel() {
 
     for(var i=0; i<currentBrandCars.length; i++)
     {
-        var newElement = '<a target="_blank" href="../../editCar.html?id=' + currentBrandCars[i].id + '"><img src="' + base_url_for_pics + currentBrandCars[i].id + '" alt="image1"/></a>'
+        //var newElement = '<a target="_blank" href="../../editCar.html?id=' + currentBrandCars[i].id + '"><img src="' + base_url_for_pics + currentBrandCars[i].id + '" alt="image1"/></a>'
+        var newElement = '<a  class="modalCaller" title="' + currentBrandCars[i].id + '"><img src="' + base_url_for_pics + currentBrandCars[i].id + '" alt="image1"/></a>'
+
         $($('#dg-container').children()[0]).append(newElement);
 
         var newElementIsotope = '<div class="grid_1_of_4 images_1_of_4">' +
-            '<a href="../../editCar.html?id=' + currentBrandCars[i].id + '">' +
+            '<a class="modalCaller" title="' + currentBrandCars[i].id + '">' +
             '<img src="' + base_url_for_pics + currentBrandCars[i].id + '">' +
             '</a>' +
-            '<a href="../../editCar.html?id=' + currentBrandCars[i].id + '">' +
+            '<a class="modalCaller" title="' + currentBrandCars[i].id + '">' +
             '<h3>'+ currentBrandCars[i].model + '</h3>' +
             '</a>' +
             '</div>';
@@ -77,6 +81,10 @@ $("#brandDropdown").change(function() {
     currentBrand = $('#brandDropdown')[0].value;
     filterCarsByBrand(currentBrand);
     populateCarousel();
+
+    $(".modalCaller").click(function () {
+        getTheModal(this.title);
+    });
     $(function () {
         $('#dg-container').gallery({
             autoplay: true
@@ -85,6 +93,43 @@ $("#brandDropdown").change(function() {
 
 });
 
+function getCarPosByID(id)
+{
+    var result = -1;
+
+    for(var i=0; i<cars.length; i++)
+    {
+        if(cars[i].id == id)
+        {
+            result = i;
+            break;
+        }
+    }
+
+    return result;
+
+}
+
+function getTheModal(title) {
+
+    console.log("getting the modal for carId= " + title);
+
+    var carPos = getCarPosByID(title);
+
+    $("#modalID").html(cars[carPos].id);
+    $("#modalModel").html(cars[carPos].model);
+    $("#modalBrand").html(cars[carPos].brand);
+    $("#modalReplica").html(cars[carPos].replica_brand);
+    $("#modalScale").html(cars[carPos].scale);
+    $("#modalDate").html(cars[carPos].date_included);
+    $("#modalComments").html(cars[carPos].comment);
+
+    $("#modalPic").attr('src', base_url_for_pics + title);
+
+    $("#CarDetailsModal").modal('show');
+
+}
+
 $(document).ready(function () {
     console.log("doc ready");
 
@@ -92,6 +137,11 @@ $(document).ready(function () {
 
         filterCarsByBrand(cars[Math.floor(Math.random() * cars.length) + 1  ].brand);
         populateCarousel();
+
+        $(".modalCaller").click(function () {
+            getTheModal(this.title);
+        });
+
         $(function () {
             $('#dg-container').gallery({
                 autoplay: true
